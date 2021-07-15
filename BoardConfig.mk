Bush-cat/android_device_samsung_gts7xlwifi

Basic device tree for y2qdcmw (SM-G986D)

BoardConfig.mk
@@ -1,0 +1,32 @@
+#
+# Copyright (C) 2020 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit common board flags
+-include device/samsung/sm8250-common/BoardConfigCommon.mk
+
+DEVICE_PATH := device/samsung/y2qdcmw
+
+# Bluetooth
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(DEVICE_PATH)/bluetooth/include
+
+# HIDL
+DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest.xml
+
+# Kernel
+TARGET_KERNEL_CONFIG := vendor/y2q_jpn_dcmw_defconfig
+
+# Keystore
+TARGET_KEYMASTER_VARIANT := samsung
+
+# Partitions
+BOARD_SUPER_PARTITION_SIZE := 8554283008
+
+# Recovery
+TARGET_RECOVERY_DENSITY := xxhdpi
+TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/etc/fstab.qcom
+
+# Inherit from proprietary vendor
+-include vendor/samsung/y2qdcmw/BoardConfigVendor.mk

device.mk
@@ -1,0 +1,40 @@
+#
+# Copyright (C) 2020 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Audio
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/audio/mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths.xml
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 3200
+TARGET_SCREEN_WIDTH := 1440
+
+# Device init scripts
+PRODUCT_PACKAGES += \
+    fstab.qcom \
+    init.recovery.target.rc
+
+# Device uses high-density artwork where available
+PRODUCT_AAPT_CONFIG := normal
+PRODUCT_AAPT_PREF_CONFIG := xxxhdpi
+
+# Keystore
+PRODUCT_PACKAGES += \
+    android.hardware.keymaster@4.0-service.samsung
+
+# Overlays
+DEVICE_PACKAGE_OVERLAYS += \
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-lineage
+
+# Shipping API
+PRODUCT_SHIPPING_API_LEVEL := 29
+
+# Inherit common device configuration
+$(call inherit-product, device/samsung/sm8250-common/common.mk)
+
+# Get non-open-source specific aspects
+$(call inherit-product, vendor/samsung/y2qdcmw/y2qdcmw-vendor.mk)

miraclestars/android_device_samsung_y2qdcmw

BoardConfig.mk
@@ -1,0 +1,32 @@
+#
+# Copyright (C) 2020 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit common board flags
+-include device/samsung/sm8250-common/BoardConfigCommon.mk
+
+DEVICE_PATH := device/samsung/y2qdcmw
+
+# Bluetooth
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(DEVICE_PATH)/bluetooth/include
+
+# HIDL
+DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest.xml
+
+# Kernel
+TARGET_KERNEL_CONFIG := vendor/lineage_y2qdcmw_defconfig
+
+# Keystore
+TARGET_KEYMASTER_VARIANT := samsung
+
+# Partitions
+BOARD_SUPER_PARTITION_SIZE := 10292822016
+
+# Recovery
+TARGET_RECOVERY_DENSITY := xxhdpi
+TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/etc/fstab.qcom
+
+# Inherit from proprietary vendor
+-include vendor/samsung/y2qdcmw/BoardConfigVendor.mk

device.mk
@@ -1,0 +1,40 @@
+#
+# Copyright (C) 2020 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Audio
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/audio/mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths.xml
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 3200
+TARGET_SCREEN_WIDTH := 1440
+
+PRODUCT_AAPT_CONFIG := large
+PRODUCT_AAPT_PREF_CONFIG := xxxhdpi
+PRODUCT_AAPT_PREBUILT_DPI := xxxhdpi xxhdpi xhdpi hdpi
+
+# Device init scripts
+PRODUCT_PACKAGES += \
+    fstab.qcom \
+    init.recovery.target.rc
+
+# Keystore
+PRODUCT_PACKAGES += \
+    android.hardware.keymaster@4.0-service.samsung
+
+# Overlays
+DEVICE_PACKAGE_OVERLAYS += \
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-lineage
+
+# Shipping API
+PRODUCT_SHIPPING_API_LEVEL := 29
+
+# Inherit common device configuration
+$(call inherit-product, device/samsung/sm8250-common/common.mk)
+
+# Get non-open-source specific aspects
+$(call inherit-product, vendor/samsung/y2qdcmw/y2qdcmw-vendor.mk)