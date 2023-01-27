6071cb9ff408836650273c223f6e6c5ec591120b
miraclestars <miracle.star.t.n.5.dec@gmail.com>
2023-01-27 22:55:39 +0900

    Initial commit from a52q Simon tree

---
+++ device.mk
@@ -0,0 +1,34 @@
+#
+# Copyright (C) 2021 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+DEVICE_PATH := device/samsung/a52q
+
+# call the common setup
+$(call inherit-product, device/samsung/sm7125-common/common.mk)
+
+# call the proprietary setup
+$(call inherit-product, vendor/samsung/a52q/a52q-vendor.mk)
+
+# Audio
+PRODUCT_COPY_FILES += \
+    $(DEVICE_PATH)/configs/audio/mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths.xml
+
+# Init files
+PRODUCT_PACKAGES += \
+    init.a52q.rc
+
+# Overlays
+DEVICE_PACKAGE_OVERLAYS += $(DEVICE_PATH)/overlay
